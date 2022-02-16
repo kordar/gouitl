1f92227feea71ebb116f4f8cdd044fba0c4c6a8d
perpel <572821520@qq.com>
2022-02-16 13:28:57 +0800

    fix uuid

--- uuid.go
+++ uuid.go
@@ -4,16 +4,15 @@ import (
 	"github.com/google/uuid"
 )
 
-var uuid4 = uuid.New()
-var uuid1, uuid1err = uuid.NewUUID()
-
 // UUID V4 基于随机数
 func UUID() string {
+	var uuid4 = uuid.New()
 	return uuid4.String()
 }
 
 // UUIDTime V1 基于时间
 func UUIDTime() string {
+	var uuid1, uuid1err = uuid.NewUUID()
 	if uuid1err != nil {
 		return UUID()
 	}
